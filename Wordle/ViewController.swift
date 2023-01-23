9c09c69751346fd046849c47006d36bf0d0b2cf6
KamilSkrzynski <kamilskrzynski95@gmail.com>
2023-01-23 15:23:54 +0100

    Initial commit

---
+++ Wordle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wordle
+//
+//  Created by Kamil Skrzyński on 23/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
